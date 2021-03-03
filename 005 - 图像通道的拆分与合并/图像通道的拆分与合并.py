96263cccd54bcf160015dd9776151e238f7c5199
foxthere <weidufox@gmail.com>
2021-03-03 22:57:44 +0300

    更新 005 - 图像通道的拆分与合并

--- 005 - 图像通道的拆分与合并/图像通道的拆分与合并.py
+++ 005 - 图像通道的拆分与合并/图像通道的拆分与合并.py
@@ -4,11 +4,16 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 
 # 创建全白的图像，3表示三个色彩通道; 第一个参数指图像大小；第二个参数指为一个图像
-img = np.zeros((255, 255, 3), np.uint8)
+# img = np.zeros((255, 255, 3), np.uint8)
+img = cv.imread("./call.png")
 
 # 通道的拆分
 b, g, r = cv.split(img)
 # print(cv.split(img))
+
+# 显示B通道的灰度图
+plt.imshow(b, cmap=plt.cm.gray)
+plt.show()
 
 # 通道的合并
 img2 = cv.merge([b, g, r])
