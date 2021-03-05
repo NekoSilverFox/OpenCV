1eb8fb3ffb50382dc4a00f867f0c9e2962c355d3
foxthere <weidufox@gmail.com>
2021-03-05 23:51:41 +0300

    增加 KP优化版虚拟绘画

--- 080 - 虚拟绘画/虚拟识别-KP优化版.py
+++ 080 - 虚拟绘画/虚拟识别-KP优化版.py
@@ -2,13 +2,12 @@ import cv2 as cv
 import numpy as np
 from stackImages import stackImages
 
-
 CAM_WIDTH = 640
 CAM_HEIGHT = 480
 CAM_BRIGHTNESS = 150
 CAM_ID = 2
 PRINT_SIZE = 5
-PRINT_COLOR = (0, 255, 255)
+PRINT_COLOR = (255, 0, 0)
 gl_path_mask = np.zeros((CAM_HEIGHT, CAM_WIDTH, 3), dtype=np.uint8)
 
 
@@ -18,10 +17,11 @@ def empty(none):
 
 def color_picker():
     # 设置摄像头的大小
-    cam = cv.VideoCapture(CAM_ID)  # 这里的数字代表摄像头ID
-    cam.set(3, CAM_WIDTH)  # 第一个参数`3`代表 `宽度`
-    cam.set(4, CAM_HEIGHT)  # 第一个参数`4`代表 `高度`
-    cam.set(10, CAM_BRIGHTNESS)  # 第一个参数`10`代表 `亮度`
+    # Настройка размера камеры
+    cam = cv.VideoCapture(CAM_ID)
+    cam.set(3, CAM_WIDTH)
+    cam.set(4, CAM_HEIGHT)
+    cam.set(10, CAM_BRIGHTNESS)
 
     # 跟踪杆
     cv.namedWindow("HSY-TrackBars")
@@ -52,6 +52,7 @@ def color_picker():
         print(h_min, h_max, s_min, s_max, v_min, v_max)
 
         # 将我们不需要的颜色设为黑色，需要的设为白色
+        # Установите нежелательные цвета на черный, а желаемые - на белый
         lower = np.array([h_min, s_min, v_min])
         upper = np.array([h_max, s_max, v_max])
         mask = cv.inRange(img_HSV, lower, upper)
@@ -63,11 +64,12 @@ def color_picker():
         cv.waitKey(1)
 
 
-def print_path(mask):
-    pass
+def print_path(x, y):
+    cv.circle(gl_img_result, (x, y), PRINT_SIZE, PRINT_COLOR, -1)
+    cv.circle(gl_path_mask, (x, y), PRINT_SIZE, PRINT_COLOR, -1)
 
 
-def find_color(img_input, prick_colors_list, print_colors_list):
+def find_color(img_input, prick_colors_list):
     img_HSV = cv.cvtColor(img_input, cv.COLOR_BGR2HSV)
     index_color = 0
 
@@ -77,15 +79,13 @@ def find_color(img_input, prick_colors_list, print_colors_list):
         mask = cv.inRange(img_HSV, lower, upper)
 
         x, y = get_contours(mask)
-        cv.circle(img_result, (x, y), PRINT_SIZE, print_colors_list[index_color], -1)
-        cv.circle(gl_path_mask, (x, y), PRINT_SIZE, print_colors_list[index_color], -1)
+        print_path(x, y)
 
         index_color += 1
         if index_color > 3:
             print("ERROR! In dead loop!")
 
         # print(mask)
-        # 将我们不需要的颜色设为黑色，需要的设为白色
         cv.imshow("Mask", mask)
 
 
@@ -96,11 +96,11 @@ def get_contours(img):
     w = 0
     h = 0
     for event in contours:
-        area = cv.contourArea(event)
-        if area > 500:
-            cv.drawContours(img_result, event, -1, (255, 0, 0), 3)
-            peri = cv.arcLength(event, True)
-            approx = cv.approxPolyDP(event, 0.02 * peri, True)
+        area_img = cv.contourArea(event)
+        if area_img > 500:
+            cv.drawContours(gl_img_result, event, -1, PRINT_COLOR, 3)
+            peri_img = cv.arcLength(event, True)
+            approx = cv.approxPolyDP(event, 0.02 * peri_img, True)
             x, y, w, h = cv.boundingRect(approx)
             print("Print point on [%d, %d]" % (x, y))
     return (x + w // 2), (y + h // 2)
@@ -111,27 +111,25 @@ def get_contours(img):
 # color_picker()
 
 # 设置摄像头的大小
-cam = cv.VideoCapture(CAM_ID)  # 这里的数字代表摄像头ID，如果有多个摄像头，请尝试更改这些ID
-cam.set(3, CAM_WIDTH)  # 第一个参数`3`代表 `宽度`
-cam.set(4, CAM_HEIGHT)  # 第一个参数`4`代表 `高度`
-cam.set(10, CAM_BRIGHTNESS)  # 第一个参数`10`代表 `亮度`
+# Настройка размера камеры
+cam = cv.VideoCapture(CAM_ID)
+cam.set(3, CAM_WIDTH)
+cam.set(4, CAM_HEIGHT)
+cam.set(10, CAM_BRIGHTNESS)
 
+# Используйте список для записи цветов, которые могут быть сопоставлены
 picker_colors_list = [[0, 103, 255, 179, 255, 255],  # Red
                       [104, 102, 134, 179, 255, 255]]  # Blue
 
-print_color_list = [[0, 0, 255],
-                    [255, 0, 0]]
-
-
 while True:
     is_success, img_row = cam.read()
-    img_result = img_row.copy()
+    gl_img_result = img_row.copy()
 
-    find_color(img_row, picker_colors_list, print_color_list)
+    find_color(img_row, picker_colors_list)
     img_result_with_path = cv.addWeighted(img_row, 1, gl_path_mask, 1, 0)
 
     # cv.imshow("Row camera", img)
-    cv.imshow("Result camera", img_result)
+    cv.imshow("Result camera", gl_img_result)
     cv.imshow("Result path on canvas", gl_path_mask)
     cv.imshow("Result camera with path", img_result_with_path)
 
